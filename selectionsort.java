06ce0282ad56c0c6c7f95bef45ba2bd507839697
satyajitbarik12345 <bariksatyajit16@gmail.com>
2023-12-22 01:33:25 +0530

    my commit

---
+++ selectionsort.java
@@ -0,0 +1,45 @@
+public class selectionsort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the found minimum element with the first element
+            swap(arr, i, minIndex);
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        System.out.println("Original array:");
+        printArray(arr);
+
+        // Perform selection sort
+        selectionSort(arr);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}
+
+
+
+            
